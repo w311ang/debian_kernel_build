dd61dbce4f3e71b9af47afb281c38bf68d530720
w311ang <30381083+w311ang@users.noreply.github.com>
2024-08-19 16:50:55 +0800

    Update post_download.sh

--- post_download.sh
+++ post_download.sh
@@ -2,4 +2,26 @@ apt-get install pip -y
 apt-get remove python3-sphinx -y
 apt-get autoremove -y
 
-pip install sphinx==2.4
+PACKAGE_NAME=python3-sphinx-rtd-theme
+mkdir -p $PACKAGE_NAME/DEBIAN
+cat <<EOF >$PACKAGE_NAME/DEBIAN/control
+Package: $PACKAGE_NAME
+Architecture: all
+Description: A dummy package
+ This is a dummy package that does nothing.
+EOF
+dpkg-deb --build $PACKAGE_NAME
+dpkg -i $PACKAGE_NAME.deb
+
+PACKAGE_NAME=python3-sphinx
+mkdir -p $PACKAGE_NAME/DEBIAN
+cat <<EOF >$PACKAGE_NAME/DEBIAN/control
+Package: $PACKAGE_NAME
+Architecture: all
+Description: A dummy package
+ This is a dummy package that does nothing.
+EOF
+dpkg-deb --build $PACKAGE_NAME
+dpkg -i $PACKAGE_NAME.deb
+
+pip install sphinx==2.4 sphinx-rtd-theme
